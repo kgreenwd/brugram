39111abdb15b657b110bd1eb94fa7bb34a3ae99c
kgreenwd <2013kgreenwood@gmail.com>
2015-07-31 01:08:54 +0200

    added commenter column to comment model.  bye felicia

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150730221229) do
+ActiveRecord::Schema.define(version: 20150730230157) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150730221229) do
     t.integer  "photo_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "commenter"
   end
 
   add_index "comments", ["photo_id"], name: "index_comments_on_photo_id", using: :btree
